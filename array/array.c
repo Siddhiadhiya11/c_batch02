890a67d6ae41904452e78995fda86ca6a19a0720
siddhi adhiya <siddhiadhiya@gmail.com>
2022-03-31 08:47:48 +0530

    commit

---
+++ array/array.c
@@ -0,0 +1,17 @@
+/* array is collection of data with the same datatype
+
+datatype array_name[size]
+int arr[10];
+
+int a; initilization
+
+int a = 10; initialize and declaration 
+ 
+a = 10; declaration
+
+
+int arr[10]=[1,2,3,4]
+             0,1,2,3  indexing in array starts from 0
+
+arr[2]
+for(i=0; i<4; i++)
